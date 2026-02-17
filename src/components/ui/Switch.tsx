9952449b99dd0d199d763bf89af92201c889668e
Nathan Beene <beene.nathan@gmail.com>
2026-02-17 13:35:06 -0600

    add Switch generic component

---
+++ src/components/ui/Switch.tsx
@@ -0,0 +1,23 @@
+import * as RSwitch from '@radix-ui/react-switch'
+
+interface SwitchProps {
+  checked?: boolean,
+  onCheckedChange?: (checked: boolean) => void,
+}
+
+const Switch = ({
+  checked = false,
+  onCheckedChange = () => {},
+}: SwitchProps) => {
+  return (
+    <RSwitch.Root
+      className='w-10 h-6 bg-fill-hover shadow-md rounded-full relative data-[state=checked]:bg-accent-active transition-colors'
+      checked={checked}
+      onCheckedChange={onCheckedChange}
+    >
+      <RSwitch.Thumb className='block w-4 h-4 bg-text rounded-full translate-x-1 data-[state=checked]:translate-x-5 transition-transform'/>
+    </RSwitch.Root>
+  )
+}
+
+export default Switch
